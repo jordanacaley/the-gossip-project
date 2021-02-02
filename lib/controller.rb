3e477cc922713ccfe4a96787cb26d48c5a1b78b9
Jordan <jordancaley10@gmail.com>
2021-02-02 17:45:53 +0100

    Link view to controller methods

--- lib/controller.rb
+++ lib/controller.rb
@@ -6,13 +6,16 @@ require 'gossip'
 require 'view'
 
 class Controller
+  attr_accessor :view
 
   def initialize
-
+    @view = View.new
   end
 
   def create_gossip
-    Gossip.new
+    params = @view.create_gossip
+    gossip = Gossip.new("Jean Michel Concierge", "Fefe est de Bordeax")
+    gossip.save
   end
 
 end
